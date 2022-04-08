81f8bfa7bd7eee215c5c975b37bd7cba78c69787
lumunge <lumungep12@gmail.com>
2022-04-09 02:10:25 +0300

    dist

--- dist/assets/ts/common.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-links");
-const mobileNav = () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-};
-hamburger.addEventListener("click", mobileNav);
--- dist/index.js
+++
@@ -1,9 +0,0 @@
-import "../src/assets/ts/common";
-import "../src/assets/ts/Grid/gridIndex";
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-links");
-const mobileNav = () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-};
-hamburger.addEventListener("click", mobileNav);
