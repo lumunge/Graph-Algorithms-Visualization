bfc26ce9d17a2ba8a0f56c1e455360dc5b7e8263
lumunge <lumungep12@gmail.com>
2022-04-09 02:05:08 +0300

    ignore

---
+++ dist/assets/ts/Grid/generateObstacles.js
@@ -0,0 +1,42 @@
+import { rowSize, colSize } from "./gridIndex";
+const obstacles = (x = 0, y = 0) => {
+    var node = document.querySelector(`div[row='${x}'][col='${y}']`);
+    if (node.classList.contains("pathNode")) {
+        return;
+    }
+    else {
+        node.classList.toggle("wall");
+        if (node.classList.contains("wall")) {
+            node.setAttribute("wall", "1");
+        }
+        else {
+            node.setAttribute("wall", "0");
+        }
+    }
+};
+export const setObstacles = () => {
+    var obstacleSlider = document.querySelector(".obstacleSlider");
+    var obstacleValue = (document.querySelector(".obstacleSlider")).value;
+    obstacleSlider.setAttribute("max", `${rowSize + colSize}`);
+    var rows = [];
+    var cols = [];
+    for (let i = 1; i < rowSize; i++) {
+        rows.push(i);
+    }
+    for (let i = 1; i < colSize; i++) {
+        cols.push(i);
+    }
+    var arr1 = rows.slice(), arr2 = cols.slice();
+    arr1.sort(function () {
+        return 0.5 - Math.random();
+    });
+    arr2.sort(function () {
+        return 0.5 - Math.random();
+    });
+    var trimVal = Math.floor((arr1.length * obstacleValue) / (rowSize + colSize));
+    arr1.splice(0, arr1.length - trimVal);
+    while (arr1.length) {
+        var rowVal = arr1.pop(), colVal = arr2[0] == rowVal ? arr2.pop() : arr2.shift();
+        obstacles(rowVal, colVal);
+    }
+};
